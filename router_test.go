a1d1c20d7f3692e26be2599fa148f9d66f37d3d2
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2020-01-21 13:12:34 +0100

    Tests refactor

--- router_test.go
+++ router_test.go
@@ -58,6 +58,32 @@ func (rw *readWriter) SetWriteDeadline(t time.Time) error {
 	return nil
 }
 
+type assertFn func(rw *readWriter)
+
+func assertWithTestServer(t *testing.T, uri string, handler fasthttp.RequestHandler, fn assertFn) {
+	s := &fasthttp.Server{
+		Handler: handler,
+	}
+
+	rw := &readWriter{}
+	ch := make(chan error)
+
+	rw.r.WriteString(uri)
+	go func() {
+		ch <- s.ServeConn(rw)
+	}()
+	select {
+	case err := <-ch:
+		if err != nil {
+			t.Fatalf("return error %s", err)
+		}
+	case <-time.After(500 * time.Millisecond):
+		t.Fatalf("timeout")
+	}
+
+	fn(rw)
+}
+
 func TestRouter(t *testing.T) {
 	router := New()
 
@@ -285,14 +311,7 @@ func TestRouterGroup(t *testing.T) {
 	r6.ServeFiles("/static/*filepath", "./")
 	r6.ServeFilesCustom("/custom/static/*filepath", &fasthttp.FS{Root: "./"})
 
-	s := &fasthttp.Server{
-		Handler: r1.Handler,
-	}
-
-	rw := &readWriter{}
-	ch := make(chan error)
-
-	requests := []string{
+	uris := []string{
 		"POST /foo HTTP/1.1\r\n\r\n",
 		// testing router group - r2 (grouped from r1)
 		"POST /boo/bar HTTP/1.1\r\n\r\n",
@@ -310,56 +329,35 @@ func TestRouterGroup(t *testing.T) {
 		"GET /moo/foo/foo/custom/static/router.go HTTP/1.1\r\n\r\n",
 	}
 
-	for _, req := range requests {
+	for _, uri := range uris {
 		hit = false
 
-		rw.r.WriteString(req)
-		go func() {
-			ch <- s.ServeConn(rw)
-		}()
-		select {
-		case err := <-ch:
-			if err != nil {
-				t.Fatalf("return error %s", err)
+		assertWithTestServer(t, uri, r1.Handler, func(rw *readWriter) {
+			br := bufio.NewReader(&rw.w)
+			var resp fasthttp.Response
+			if err := resp.Read(br); err != nil {
+				t.Fatalf("Unexpected error when reading response: %s", err)
 			}
-		case <-time.After(100 * time.Millisecond):
-			t.Fatalf("timeout")
-		}
+			if !(resp.Header.StatusCode() == fasthttp.StatusOK) {
+				t.Fatalf("Status code %d, want %d", resp.Header.StatusCode(), fasthttp.StatusOK)
+			}
+			if !strings.Contains(uri, "static") && !hit {
+				t.Fatalf("Regular routing failed with router chaining. %s", uri)
+			}
+		})
+	}
+
+	assertWithTestServer(t, "POST /qax HTTP/1.1\r\n\r\n", r1.Handler, func(rw *readWriter) {
 		br := bufio.NewReader(&rw.w)
 		var resp fasthttp.Response
 		if err := resp.Read(br); err != nil {
 			t.Fatalf("Unexpected error when reading response: %s", err)
 		}
-		if !(resp.Header.StatusCode() == fasthttp.StatusOK) {
-			t.Fatalf("Status code %d, want %d", resp.Header.StatusCode(), fasthttp.StatusOK)
+		if !(resp.Header.StatusCode() == fasthttp.StatusNotFound) {
+			t.Errorf("NotFound behavior failed with router chaining.")
+			t.FailNow()
 		}
-		if !strings.Contains(req, "static") && !hit {
-			t.Fatalf("Regular routing failed with router chaining. %s", req)
-		}
-	}
-
-	// Not found
-	rw.r.WriteString("POST /qax HTTP/1.1\r\n\r\n")
-	go func() {
-		ch <- s.ServeConn(rw)
-	}()
-	select {
-	case err := <-ch:
-		if err != nil {
-			t.Fatalf("return error %s", err)
-		}
-	case <-time.After(100 * time.Millisecond):
-		t.Fatalf("timeout")
-	}
-	br := bufio.NewReader(&rw.w)
-	var resp fasthttp.Response
-	if err := resp.Read(br); err != nil {
-		t.Fatalf("Unexpected error when reading response: %s", err)
-	}
-	if !(resp.Header.StatusCode() == fasthttp.StatusNotFound) {
-		t.Errorf("NotFound behavior failed with router chaining.")
-		t.FailNow()
-	}
+	})
 }
 
 func TestRouterOPTIONS(t *testing.T) {
@@ -753,37 +751,19 @@ func TestRouterServeFiles(t *testing.T) {
 
 	r.ServeFiles("/*filepath", os.TempDir())
 
-	s := &fasthttp.Server{
-		Handler: r.Handler,
-	}
-
-	rw := &readWriter{}
-	ch := make(chan error)
-
-	rw.r.WriteString(string("GET /favicon.ico HTTP/1.1\r\n\r\n"))
-	go func() {
-		ch <- s.ServeConn(rw)
-	}()
-	select {
-	case err := <-ch:
-		if err != nil {
-			t.Fatalf("return error %s", err)
+	assertWithTestServer(t, "GET /favicon.ico HTTP/1.1\r\n\r\n", r.Handler, func(rw *readWriter) {
+		br := bufio.NewReader(&rw.w)
+		var resp fasthttp.Response
+		if err := resp.Read(br); err != nil {
+			t.Fatalf("Unexpected error when reading response: %s", err)
 		}
-	case <-time.After(500 * time.Millisecond):
-		t.Fatalf("timeout")
-	}
-
-	br := bufio.NewReader(&rw.w)
-	var resp fasthttp.Response
-	if err := resp.Read(br); err != nil {
-		t.Fatalf("Unexpected error when reading response: %s", err)
-	}
-	if resp.Header.StatusCode() != 200 {
-		t.Fatalf("Unexpected status code %d. Expected %d", resp.Header.StatusCode(), 423)
-	}
-	if !bytes.Equal(resp.Body(), body) {
-		t.Fatalf("Unexpected body %q. Expected %q", resp.Body(), string(body))
-	}
+		if resp.Header.StatusCode() != 200 {
+			t.Fatalf("Unexpected status code %d. Expected %d", resp.Header.StatusCode(), 423)
+		}
+		if !bytes.Equal(resp.Body(), body) {
+			t.Fatalf("Unexpected body %q. Expected %q", resp.Body(), string(body))
+		}
+	})
 }
 
 func TestRouterServeFilesCustom(t *testing.T) {
@@ -806,37 +786,19 @@ func TestRouterServeFilesCustom(t *testing.T) {
 
 	r.ServeFilesCustom("/*filepath", fs)
 
-	s := &fasthttp.Server{
-		Handler: r.Handler,
-	}
-
-	rw := &readWriter{}
-	ch := make(chan error)
-
-	rw.r.WriteString(string("GET /favicon.ico HTTP/1.1\r\n\r\n"))
-	go func() {
-		ch <- s.ServeConn(rw)
-	}()
-	select {
-	case err := <-ch:
-		if err != nil {
-			t.Fatalf("return error %s", err)
+	assertWithTestServer(t, "GET /favicon.ico HTTP/1.1\r\n\r\n", r.Handler, func(rw *readWriter) {
+		br := bufio.NewReader(&rw.w)
+		var resp fasthttp.Response
+		if err := resp.Read(br); err != nil {
+			t.Fatalf("Unexpected error when reading response: %s", err)
 		}
-	case <-time.After(500 * time.Millisecond):
-		t.Fatalf("timeout")
-	}
-
-	br := bufio.NewReader(&rw.w)
-	var resp fasthttp.Response
-	if err := resp.Read(br); err != nil {
-		t.Fatalf("Unexpected error when reading response: %s", err)
-	}
-	if resp.Header.StatusCode() != 200 {
-		t.Fatalf("Unexpected status code %d. Expected %d", resp.Header.StatusCode(), 200)
-	}
-	if !bytes.Equal(resp.Body(), body) {
-		t.Fatalf("Unexpected body %q. Expected %q", resp.Body(), string(body))
-	}
+		if resp.Header.StatusCode() != 200 {
+			t.Fatalf("Unexpected status code %d. Expected %d", resp.Header.StatusCode(), 200)
+		}
+		if !bytes.Equal(resp.Body(), body) {
+			t.Fatalf("Unexpected body %q. Expected %q", resp.Body(), string(body))
+		}
+	})
 }
 
 func TestRouterList(t *testing.T) {
